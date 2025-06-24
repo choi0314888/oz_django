1928b4ec73625d3e79462cfc996b2a6250e60407
최재현 <zip020314@gmail.com>
2025-06-23 21:43:14 -0700

    3

--- boards/admin.py
+++ boards/admin.py
@@ -3,4 +3,20 @@ from .models import Board
 # Register your models here.
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
-	pass
+    list_display = ('title', 'writer', 'date', 'likes', 'content', 'create_at', 'updated_at')
+    list_filter = ('date', 'writer')
+    search_fields = ('title', 'content')
+    ordering = ('-date',)
+    readonly_fields = ('writer',)
+    fieldsets = (
+        (None, {'fields': ('title', 'content')}),
+        ('추가 옵션', {'fields': ('writer', 'likes', 'reviews'), 'classes': ('collapse',)}),
+    )
+    list_per_page = 5
+    actions = ('increment_likes',)
+    
+def increment_likes(self, request, queryset):
+    for board in queryset:
+        board.like += 1
+        board.save()
+increment_likes.short_description = "선택한 게시글의 좋아요 수 증가"
